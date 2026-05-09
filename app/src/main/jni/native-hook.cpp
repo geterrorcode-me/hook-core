6222b76f82f637c8b967da097b704278f757268f
getError <geterrorcode@gmail.com>
2026-05-09 18:50:05 +0700

    Memalsukan PID

--- app/src/main/jni/native-hook.cpp
+++ app/src/main/jni/native-hook.cpp
@@ -6,13 +6,12 @@
 #define LOG_TAG "BlackHook-Native"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-// 1. Siapkan variabel untuk menyimpan alamat fungsi asli
+// 1. Variabel untuk menyimpan alamat fungsi asli (agar aplikasi tidak crash)
 int (*orig_getpid)();
 
-// 2. Buat fungsi "Palsu" (Hook Function)
+// 2. Fungsi "Palsu" kita
 int fake_getpid() {
-    LOGI("Aksi Terdeteksi: Fungsi getpid() dipanggil, tapi BlackHook membelokkannya!");
-    // Kita kembalikan nilai palsu, misalnya 8888
+    LOGI("Diterjang BlackHook: Seseorang memanggil getpid(), kita berikan nilai palsu!");
     return 8888; 
 }
 
@@ -25,13 +24,13 @@ Java_black_hook_HookManager_getEngineVersion(JNIEnv *env, jclass clazz) {
 
 JNIEXPORT void JNICALL
 Java_black_hook_HookManager_nativeHookMethod(JNIEnv *env, jclass clazz, jobject method) {
-    LOGI("Mencoba memasang Inline Hook pada getpid...");
+    LOGI("Memasang Inline Hook pada getpid...");
 
     // 3. Eksekusi Hook menggunakan Dobby
-    // Kita ambil alamat fungsi getpid asli, belokkan ke fake_getpid, simpan aslinya di orig_getpid
+    // Parameter: (Alamat Fungsi Target, Fungsi Pengganti, Simpan Fungsi Asli)
     DobbyHook((void *)getpid, (void *)fake_getpid, (void **)&orig_getpid);
 
-    LOGI("Hook Berhasil! Sekarang getpid() akan selalu mengembalikan 8888");
+    LOGI("Hook Berhasil Pasang! Sistem sekarang akan menganggap PID adalah 8888");
 }
 
 }
